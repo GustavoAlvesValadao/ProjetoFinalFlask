aa9a122854397d39e33674fe2d75cd550cdee40e
Gustavo Alves <gustavovaladao.4491@aluno.saojudas.br>
2022-09-22 17:50:41 -0300

    adicionando pastas e rotas

--- __init__.py
+++ __init__.py
@@ -1,12 +1,17 @@
 from flask import Flask 
+from .extensions import db, migrate
+#adiciona isso
+from .routes.ucBp import ucBp
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    db.init_app(app)
+    migrate.init_app(app)
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    #e adiciona isso
+    app.register_blueprint(ucBp)
 
     return app
